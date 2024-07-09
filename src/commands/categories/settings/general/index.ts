c8732579a05b97610cbb91c2247804e8a21b8138
Ayomi <nikitakabo74@gmail.com>
2024-07-10 01:13:04 +0300

    refactor: use destructuriztation

--- src/commands/categories/settings/general/index.ts
+++ src/commands/categories/settings/general/index.ts
@@ -1,6 +1,6 @@
 import BaseSelectMenuValue from "@src/abstractions/BaseSelectMenuValue";
 import { StringSelectMenuInteraction } from "discord.js";
-import { traditionEmbedGenerator } from "./embedGenerator";
+import { generalEmbedGenerator } from "./embedGenerator";
 
 // TODO: перевести все сообщения
 
@@ -8,10 +8,9 @@ export class GeneralSettings extends BaseSelectMenuValue {
   value: string = "global";
   async execute(interaction: StringSelectMenuInteraction) {
     await interaction.deferReply({ ephemeral: true });
-    const data = await traditionEmbedGenerator(interaction.guild);
+    const data = await generalEmbedGenerator(interaction.guild);
     return interaction.editReply({
-      embeds: [data.embed],
-      components: [data.verificationTypeSelect],
+      ...data
     });
   }
 }
